2596efbc2511e6a359c1cbc419f08a28ff0cff73
Mohamed ABRACHE <abrache2001@gmail.com>
2024-01-13 23:46:11 +0100

    structuring the features folders

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import configuration from './config/configuration';
+import { DatabaseConfig } from './config/database.config';
+
+import { DepartmentsModule } from './departments/departments.module';
+import { ServicesModule } from './services/services.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { CategoriesModule } from './categories/categories.module';
 
 
 @Module({
-  imports: [],
+  imports: [ 
+    ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+    load: [configuration],
+  }),
+
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useClass: DatabaseConfig,
+  }),DepartmentsModule, ServicesModule, ReservationsModule, CategoriesModule],
   controllers: [],
   providers: [],
 })
---
+++ apps/server/src/config/configuration.ts
@@ -0,0 +1,15 @@
+export default () => ({
+  port: Number(process.env.PORT) || 8080,
+  jwtSecret: process.env.JWT_SECRET,
+  postgresDB: {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    logging: false,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  },
+});
