06b9b821aa5720746cea7dabd26d20c6a179bc3f
Mohamed ABRACHE <abrache2001@gmail.com>
2024-02-01 16:11:46 +0100

    adding the technician crud

--- apps/server/src/acounts/technicians/technicians.controller.ts
+++ apps/server/src/acounts/technicians/technicians.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { TechniciansService } from './technicians.service';
 import { CreateTechnicianDto } from './dto/create-technician.dto';
 import { UpdateTechnicianDto } from './dto/update-technician.dto';
+import { Technician } from './entities/technician.entity';
+import { FindOneOptions } from 'typeorm';
 
 @Controller('technicians')
 export class TechniciansController {
@@ -12,23 +14,41 @@ export class TechniciansController {
     return this.techniciansService.create(createTechnicianDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('all')
+  getAll() {
     return this.techniciansService.findAll();
   }
 
+  @Get()
+  findUntrashed() {
+    const options: FindOneOptions<Technician> = { where: { isDeleted: false } };
+    return this.techniciansService.find(options); 
+  }
+
+  @Get('trash')
+  findTrashed(){
+    const options: FindOneOptions<Technician> = { where: { isDeleted: true } };
+    return this.techniciansService.find(options);  
+  }
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.techniciansService.findOne(+id);
+    const options: FindOneOptions<Technician> = { where: { id: id , isDeleted: false } };
+    return this.techniciansService.find(options);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTechnicianDto: UpdateTechnicianDto) {
-    return this.techniciansService.update(+id, updateTechnicianDto);
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateTechnicianDto: UpdateTechnicianDto) {
+    return this.techniciansService.update({ where: { id: id  } }, updateTechnicianDto);
   }
-
+  
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.techniciansService.remove(+id);
+  trash(@Param('id') id: string) {
+    return this.techniciansService.softDelete(id);
+  }
+
+  @Delete('forcedelete/:id')
+  forceDelete(@Param('id') id: string) {
+    return this.techniciansService.forceDelete(id);
   }
 }
--- apps/server/src/acounts/technicians/technicians.module.ts
+++ apps/server/src/acounts/technicians/technicians.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TechniciansService } from './technicians.service';
 import { TechniciansController } from './technicians.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Technician } from './entities/technician.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Technician]),
+  ],
   controllers: [TechniciansController],
   providers: [TechniciansService],
 })
