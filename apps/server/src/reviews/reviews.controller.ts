6ecadcbac746f318843e8b1a5fe97ecd0a5651aa
Mohamed ABRACHE <abrache2001@gmail.com>
2024-02-01 16:33:30 +0100

    ading the review crud

--- apps/server/src/reviews/reviews.controller.ts
+++ apps/server/src/reviews/reviews.controller.ts
@@ -1,34 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { Review } from './entities/review.entity';
+import { FindOneOptions } from 'typeorm';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
-
   @Post()
   create(@Body() createReviewDto: CreateReviewDto) {
     return this.reviewsService.create(createReviewDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('all')
+  getAll() {
     return this.reviewsService.findAll();
   }
 
+  @Get()
+  findUntrashed() {
+    const options: FindOneOptions<Review> = { where: { isDeleted: false } };
+    return this.reviewsService.find(options); 
+  }
+
+  @Get('trash')
+  findTrashed(){
+    const options: FindOneOptions<Review> = { where: { isDeleted: true } };
+    return this.reviewsService.find(options);  
+  }
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+    const options: FindOneOptions<Review> = { where: { id: id , isDeleted: false } };
+    return this.reviewsService.find(options);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewsService.update(+id, updateReviewDto);
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
+    return this.reviewsService.update({ where: { id: id  } }, updateReviewDto);
   }
-
+  
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewsService.remove(+id);
+  trash(@Param('id') id: string) {
+    return this.reviewsService.softDelete(id);
+  }
+
+  @Delete('forcedelete/:id')
+  forceDelete(@Param('id') id: string) {
+    return this.reviewsService.forceDelete(id);
   }
 }
