08edd9312ea5a929754b474c1aa736fd0c962f13
Mohamed ABRACHE <abrache2001@gmail.com>
2024-02-01 16:53:43 +0100

    server: ading the review crud starter

--- apps/server/src/acounts/administrators/administrators.controller.ts
+++ apps/server/src/acounts/administrators/administrators.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { AdministratorsService } from './administrators.service';
 import { CreateAdministratorDto } from './dto/create-administrator.dto';
 import { UpdateAdministratorDto } from './dto/update-administrator.dto';
+import { FindOneOptions } from 'typeorm';
+import { Administrator } from './entities/administrator.entity';
 
 @Controller('administrators')
 export class AdministratorsController {
@@ -11,24 +13,42 @@ export class AdministratorsController {
   create(@Body() createAdministratorDto: CreateAdministratorDto) {
     return this.administratorsService.create(createAdministratorDto);
   }
-
-  @Get()
-  findAll() {
+  
+  @Get('all')
+  getAll() {
     return this.administratorsService.findAll();
   }
-
+  
+  @Get()
+  findUntrashed() {
+    const options: FindOneOptions<Administrator> = { where: { isDeleted: false } };
+    return this.administratorsService.find(options); 
+  }
+  
+  @Get('trash')
+  findTrashed(){
+    const options: FindOneOptions<Administrator> = { where: { isDeleted: true } };
+    return this.administratorsService.find(options);  
+  }
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.administratorsService.findOne(+id);
+    const options: FindOneOptions<Administrator> = { where: { id: id , isDeleted: false } };
+    return this.administratorsService.find(options);
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdministratorDto: UpdateAdministratorDto) {
-    return this.administratorsService.update(+id, updateAdministratorDto);
+  
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateAdministratorDto: UpdateAdministratorDto) {
+    return this.administratorsService.update({ where: { id: id  } }, updateAdministratorDto);
   }
-
+  
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.administratorsService.remove(+id);
+  trash(@Param('id') id: string) {
+    return this.administratorsService.softDelete(id);
+  }
+  
+  @Delete('forcedelete/:id')
+  forceDelete(@Param('id') id: string) {
+    return this.administratorsService.forceDelete(id);
   }
 }
